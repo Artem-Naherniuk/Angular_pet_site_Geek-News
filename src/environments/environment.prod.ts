7e999693efe3be9420b53e42d803566caac07869
Aperonis <aperonis16@gmail.com>
2021-04-08 22:40:26 +0300

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCBMQIGqOlfNCBLhx_PdN5jPht3DRTvgj8',
+    authDomain: 'geek-news-946fe.firebaseapp.com',
+    projectId: 'geek-news-946fe',
+    storageBucket: 'geek-news-946fe.appspot.com',
+    messagingSenderId: '102755145653',
+    appId: '1:102755145653:web:7e304935d9f7b1b01debfc'
+  }
 };
